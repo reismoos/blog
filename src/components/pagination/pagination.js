8a2a40416a0a338c7feec32f8db16f8cc0ebb8a9
Dmitry <Reismoos@gmail.om>
2023-09-04 13:33:01 +0300

    correct pagination

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,17 +1,23 @@
 import { Pagination } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 import { changePage } from '../posts-list/posts-slice'
 
 const MyPagination = () => {
   const dispatch = useDispatch()
   const { postsCount, currentPage } = useSelector((state) => state.posts)
+  useEffect(() => {
+    const page = sessionStorage.getItem('page')
+    dispatch(changePage(+page))
+  }, [])
   const onChangePage = (page) => {
     dispatch(changePage(page))
+    sessionStorage.setItem('page', page)
   }
+  console.log(currentPage)
   return (
     <Pagination
-      defaultCurrent={1}
       current={currentPage}
       onChange={onChangePage}
       total={postsCount}
